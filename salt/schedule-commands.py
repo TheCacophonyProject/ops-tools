27c6600b363d1fcd63a2d95d90c4f98f72f832bb
cam <cameron.ryan.pears@gmail.com>
2022-04-19 17:11:43 +1200

    Added salt schedule commands

---
+++ salt/schedule-commands.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python -u
+
+"""Automatically run commands on devices when they connect"""
+
+from __future__ import print_function
+
+from itertools import imap, ifilter
+import sys
+import shlex
+
+import salt.client
+
+from salt_listener import SaltListener
+
+COMMAND_FILE = "/opt/ops-tools/salt/commands.txt"
+
+def main():
+    print("creating Salt client")
+    salt_client = salt.client.LocalClient(auto_reconnect=True)
+
+    # listen to salt-master events, filter out anything other than
+    # minion ping events
+    ping_events = imap(match_minion_ping, iter(SaltListener()))
+    minion_ids = ifilter(bool, ping_events)
+
+    #minion_ids = ["pi-1755"]    # For testing
+
+    print("listening for minion ping events")
+    for minion_id in minion_ids:
+        line = getMinionCommand(minion_id)
+        while line != None:
+            print("Line:", line.strip())
+            split = shlex.split(line)
+            minion_id = split[0]
+            command = split[1]
+            args = split[2:] if 2 < len(split) else []
+            print("Running command. Minion:", minion_id, ", Command:", command,", Args:", args)
+            job = salt_client.cmd(minion_id, command, args)
+            print("Result: ("+line.strip()+"):", job)
+
+            deleteMinionCommand(line)
+            line = getMinionCommand(minion_id)
+
+def getMinionCommand(minion_id):
+    with open(COMMAND_FILE, "r") as file:
+        lines = file.readlines()
+    for line in lines:
+        words = line.strip().split()
+        if len(words) == 0:
+            continue
+        if words[0] == minion_id:
+            return line
+    return None
+
+def deleteMinionCommand(line):
+    with open(COMMAND_FILE, "r") as fr:
+        lines = fr.readlines()
+    lines.remove(line)
+    with open(COMMAND_FILE, "w") as fw:
+        for l in lines:
+            fw.write(l)
+
+def match_minion_ping(event):
+    if event is None:
+        return None
+
+    tag = event.get("tag", "")
+    if tag == "salt/event/exit":
+        sys.exit()
+
+    if tag == "minion_ping":
+        data = event.get("data")
+        if not data:
+            return None
+        return data.get("id")
+    return None
+
+if __name__ == "__main__":
+    main()
