5d8bf516f5b9f2a26e08ee588339a42ff90de895
cam <cameron.ryan.pears@gmail.com>
2023-06-21 23:01:46 +1200

    Update object-backup WIP

--- backups/object-backup/object-backup.py
+++ backups/object-backup/object-backup.py
@@ -1,102 +1,57 @@
 #!/usr/bin/python3
+import boto3
+import botocore
+import concurrent.futures
 import yaml
-import os
-import sys
-import subprocess
-from influxdb import InfluxDBClient
-import socket
+import threading
 
-HOST_NAME = socket.gethostname()
-CONFIG_FILE = "./object-backup.yaml"
-MC = '/usr/local/bin/mc'
+with open('object-backup.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
-exp = {
-    "T": 1000000000000,
-    "G": 1000000000,
-    "M": 1000000,
-    "K": 1000,
-    "B": 1,
-}
+# Local S3 storage
+local_s3 = boto3.resource('s3',
+    aws_access_key_id=config['local']['aws_access_key_id'],
+    aws_secret_access_key=config['local']['aws_secret_access_key'],
+    endpoint_url=config['local']['endpoint_url'])
+local_bucket = local_s3.Bucket(config['local']['bucket'])
 
-def data_count(keyword, data):
-    i = data.index(keyword)
-    if i == -1:
-        print(f"failed to parse '{keyword}' in '{data}'")
-        sys.exit()
-    return float(data[i+1]) * exp[data[i+2][0]]
+# Archive S3 storage
+archive_s3 = boto3.resource('s3',
+    aws_access_key_id=config['archive']['aws_access_key_id'],
+    aws_secret_access_key=config['archive']['aws_secret_access_key'],
+    endpoint_url=config['archive']['endpoint_url'])
+archive_bucket = archive_s3.Bucket(config['archive']['bucket'])
 
-dry_run = False
-if len(sys.argv) > 1:
-    if sys.argv[1] != "--dry-run":
-        print(f"Unknown parameter {sys.argv[1]}")
-        sys.exit()
-    else:
-        print("Doing a dry run")
-        dry_run = True
+# Copies a file from a local S3 bucket to an archive S3 bucket if it does not exist in the archive bucket,
+# and verifies that the object exists in both buckets. If the object exists but has been modified,
+# raises an error.
+def handle_file(obj):
+    try:
+        local_obj = local_s3.Object(config['local']['bucket'], obj.key)
+        try:
+            archive_obj = archive_s3.Object(config['archive']['bucket'], obj.key)
+            archive_obj.load()
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                # The object does not exist, so we copy it
+                print(f"copying file {obj.key} to archive bucket")
+                archive_obj.put(Body=local_obj.get()['Body'].read())
+            else:
+                # Something else has gone wrong
+                raise
+        else:
+            if obj.e_tag != archive_obj.e_tag:
+                print(f"Error: object {obj.key} has been modified.")
+            print(f"Object {obj.key} exists in both buckets. Skipping.")
+    
+    except Exception as e:
+        print(f"Error handling object {obj.key}: {e}")
 
-if not os.path.exists(CONFIG_FILE):
-    print(f"failed to find config file '{CONFIG_FILE}'")
-    sys.exit()
 
-with open(CONFIG_FILE, 'r') as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+# Use a ThreadPoolExecutor to handle multiple files at once
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    for obj in local_bucket.objects.page_size(1000):
+        if obj.key.endswith("-thumbnail"):
+            continue
+        executor.submit(handle_file, obj)
 
-localstore = config['localstore']
-remotestore = os.path.join(config['remotestore'], config['remotePrefix'])
-
-mc_command = ["/usr/local/bin/mc", "mirror", "--quiet", "--exclude", "*-thumb", localstore, remotestore]
-if dry_run:
-    mc_command.insert(mc_command.index("mirror")+1, "--fake")
-
-print(f"running '{' '.join(mc_command)}'")
-
-out = subprocess.check_output(mc_command)
-out = out.decode("utf-8")
-lines = out.split("\n")
-
-total_files = 0
-data_total = 0
-data_transferred = 0
-data_speed = 0
-
-success = 1
-for line in lines:
-    if line.__contains__('Failed'):
-        success = 0
-        print(line)
-    if len(line) == 0:
-        continue
-    if line.startswith(f"`{localstore}"):
-        total_files += 1
-    elif line.startswith("Total"):
-        parts = line.split(" ")
-        data_total = data_count("Total:", parts)
-        data_transferred = data_count("Transferred:", parts)
-        data_speed = data_count("Speed:", parts)
-    else:
-        print(f"unknown line: {line}")
-        sys.exit()
-
-print("Logging to influx")
-json_body = [{
-        "measurement": "object_backup",
-        "tags": {
-            "host": HOST_NAME,
-            "object_storage": remotestore
-        },
-        "fields": {
-            "success": float(success),
-            "data_total": data_total,
-            "data_transferred": data_transferred,
-            "data_speed": data_speed,
-            "total_files": total_files,
-        }
-    }]
-client = InfluxDBClient(**config['influx'])
-print(json_body)
-if dry_run:
-    print("Skipping reporting to influx")
-else:
-    client.write_points(json_body)
-
-print("Finished objectstorage backup")
